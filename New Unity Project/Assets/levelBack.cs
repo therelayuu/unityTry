77735faa31ee9eddffe0b464e152453b6a7a127e
therelayuu <quadrox3@icloud.com>
2021-02-25 01:19:06 +0300

    push

---
+++ New Unity Project/Assets/levelBack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelBack : MonoBehaviour
+{
+    public string levelName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
